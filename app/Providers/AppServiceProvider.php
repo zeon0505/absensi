94e3cf6d8359576c5068a27ff8ed9373da91c465
zeon0505 <zeon0505@example.com>
2026-03-21 00:42:06 +0700

    s

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -19,6 +19,10 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
+        if (config('app.env') === 'production') {
+            \Illuminate\Support\Facades\URL::forceScheme('https');
+        }
+
         \Illuminate\Support\Facades\Gate::define('admin', function (\App\Models\User $user) {
             return $user->role === 'admin';
         });
